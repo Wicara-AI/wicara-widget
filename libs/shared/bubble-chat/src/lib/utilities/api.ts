8b638829dd077654667d82fea33b601794831532
Aldy Rifaldi.B <52684373+aldyrifaldi@users.noreply.github.com>
2024-08-09 00:17:16 +0700

    feat: Implement user registration functionality in RegisterForm component

---
+++ libs/shared/bubble-chat/src/lib/utilities/api.ts
@@ -0,0 +1,29 @@
+import { ApiResponse, ResponseSuccess } from "../types/api";
+import { BaseApiRequest } from "./baseApi";
+
+export type RegisterUserRequest = {
+  email: string;
+  name: string;
+};
+
+export type RegisterUserResponseData = {
+  id: string;
+};
+
+const baseApi = new BaseApiRequest('http://localhost:3000');
+
+export const registerUser = async (data: RegisterUserRequest, signal: AbortSignal) => {
+    const response = await baseApi.post('/widget/register', data, signal);
+
+    if (!response.ok) {
+        throw new Error('Failed to register user');
+    }
+
+    const body = await response.json() as ApiResponse<RegisterUserResponseData>;
+
+    if (body.status === "fail") {
+        throw new Error(body.message);
+    }
+
+    return body.data;
+};
