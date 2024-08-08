5ac8fe5765848201782b1c5983a484c7a837040f
Aldy Rifaldi.B <52684373+aldyrifaldi@users.noreply.github.com>
2024-08-08 23:51:16 +0700

    refactor: Update RegisterForm component and styles

--- libs/shared/bubble-chat/src/lib/partials/RegisterForm.tsx
+++ libs/shared/bubble-chat/src/lib/partials/RegisterForm.tsx
@@ -1,28 +1,51 @@
-import { useState } from "react";
-import RegisterButtonSubmit from "../components/forms/RegisterButtonSubmit";
-import TextInput from "../components/inputs/TextInput";
+import RegisterButtonSubmit from '../components/forms/RegisterButtonSubmit'
+import TextInput from '../components/inputs/TextInput'
+import styles from './register-form.module.css'
+import { FormGroup, FormLabel } from '../components/inputs/FormGroup'
+import { useForm } from '../hooks/useForm'
 
 interface RegisterFormDto {
-  email: string;
-  password: string;
-  confirmPassword: string;
+  email: string
+  name: string
 }
 
 export default function RegisterForm() {
-  const [inputState, setInputState] = useState<RegisterFormDto>({
-    email: '',
-    password: '',
-    confirmPassword: ''
-  });
+  const form = useForm<RegisterFormDto>({
+    initialValues: {
+      email: '',
+      name: '',
+    },
+  })
 
-  const handleSubmit = () => {
-    console.log(inputState);
+  const handleSubmit = (data: RegisterFormDto) => {
+    console.log(data)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextInput name="email" type="text" />
-        <RegisterButtonSubmit type="submit">Register</RegisterButtonSubmit>
+    <form onSubmit={form.handleSubmit(handleSubmit)}>
+      <div className={styles['form-container']}>
+        <FormGroup>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <TextInput
+            id="email"
+            name="email"
+            type="email"
+            onChange={(event) => form.setValue('email', event.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <FormLabel htmlFor="name">Nama Lengkap</FormLabel>
+          <TextInput
+            id="name"
+            name="name"
+            type="text"
+            onChange={(event) => form.setValue('name', event.target.value)}
+          />
+        </FormGroup>
+        <div>
+          <RegisterButtonSubmit type="submit">Register</RegisterButtonSubmit>
+        </div>
+      </div>
     </form>
-  );
+  )
 }
