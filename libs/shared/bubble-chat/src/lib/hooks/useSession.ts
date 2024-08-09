31e23772eaafadf7681a11ac31556cadb0cd8439
Aldy Rifaldi.B <52684373+aldyrifaldi@users.noreply.github.com>
2024-08-09 18:39:48 +0700

    refactor: Use crypto.randomUUID() to generate session ID in useSession hook

--- libs/shared/bubble-chat/src/lib/hooks/useSession.ts
+++ libs/shared/bubble-chat/src/lib/hooks/useSession.ts
@@ -11,10 +11,7 @@ export const useSession = () => {
   }
 
   const generateSession = () => {
-    return (
-      Math.random().toString(36).substring(2, 15) +
-      Math.random().toString(36).substring(2, 15)
-    )
+    return crypto.randomUUID();
   }
 
   const generateSessionIfNotAvailable = () => {
